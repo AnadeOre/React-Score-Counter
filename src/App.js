f60cdc32ab9d183c7b14fe1f69f4b43ed0c9fe9b
Ana <anaemilia.deorellana@gmail.com>
2021-03-14 17:33:28 -0300

    fixed useEffect error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import GameSettings from './GameSettings';
 import PlayerList from './PlayerList';
